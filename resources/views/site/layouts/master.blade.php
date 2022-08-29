8eb9bc185bcdf4dc842afc4c095d4b8c63c3c9d0
thangg <thangnguyennvt1410@gmail.com>
2022-08-29 15:38:29 +0700

    f

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -79,10 +79,7 @@
         animateIn: 'fadeIn',
         animateOut: 'fadeOut',
         autoplayTimeout: 2000,
-        navText: [
-            '<svg class="left" width="50" height="50" viewBox="0 0 24 24" style="left: -620"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
-            '<svg class="right" width="50" height="50" viewBox="0 0 24 24" style="right: -620"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>' /* icons from https://iconmonstr.com */
-        ]
+        navText: ["", ""]
     });
 
 </script>
